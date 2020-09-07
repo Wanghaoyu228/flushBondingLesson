b0b510a44128c494692333a803d165d099c71000
2860922361 <ZQM19911207>
2020-09-07 08:34:15 +0800

    first commit

---
+++ src/main/java/com/hwc/flushBondingLesson/service/KnowledgeService.java
@@ -0,0 +1,29 @@
+package com.hwc.flushBondingLesson.service;
+
+import com.hwc.flushBondingLesson.bean.Knowledge;
+import com.hwc.flushBondingLesson.mapper.KnowledgeMapper;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Service
+public class KnowledgeService {
+
+    @Resource
+    private KnowledgeMapper knowledgeMapper;
+
+
+
+
+
+    //列表
+    public List<Knowledge> findAllKnowledge(String level){
+        return knowledgeMapper.findAllKnowledge(level);
+    }
+
+
+    public Knowledge findOneById(int id) {
+        return knowledgeMapper.findOneById(id);
+    }
+}
